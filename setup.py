92c63ea46235359b60629dc65eef5ee596138d9e
AJ Hawks <aj.hawks@mapmyfitness.com>
2013-02-25 11:51:14 -0700

    Set version to 0.0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='silverpop',
-    version='0.0.2',
+    version='0.0.2.1',
     description='Silverpop API wrapper.',
     author='Thomas Welfley',
     author_email='thomas@yola.com',
